78c6ef8b5a6d91f7039c4937938cb7e4d1a6cf09
CKH <1991652575@qq.com>
2025-08-04 14:59:17 +0800

    Create keelung-real-data.js

---
+++ keelung-real-data.js
@@ -0,0 +1,74 @@
+/**
+ * 基隆市2024年大选真实数据
+ * 数据来源：中选会原始数据
+ */
+
+// 基隆市2024年大选真实数据
+const keelungRealData = {
+    "中正區": {
+        "kmt_votes": 11700,
+        "dpp_votes": 10241,
+        "tpp_votes": 8002,
+        "total_votes": 29943,
+        "kmt_rate": 39.1,
+        "dpp_rate": 34.2,
+        "tpp_rate": 26.7
+    },
+    "七堵區": {
+        "kmt_votes": 11577,
+        "dpp_votes": 11554,
+        "tpp_votes": 8632,
+        "total_votes": 31763,
+        "kmt_rate": 36.4,
+        "dpp_rate": 36.4,
+        "tpp_rate": 27.2
+    },
+    "暖暖區": {
+        "kmt_votes": 9033,
+        "dpp_votes": 7673,
+        "tpp_votes": 6583,
+        "total_votes": 23289,
+        "kmt_rate": 38.8,
+        "dpp_rate": 32.9,
+        "tpp_rate": 28.3
+    },
+    "仁愛區": {
+        "kmt_votes": 8954,
+        "dpp_votes": 9266,
+        "tpp_votes": 5745,
+        "total_votes": 23965,
+        "kmt_rate": 37.3,
+        "dpp_rate": 38.6,
+        "tpp_rate": 24.0
+    },
+    "中山區": {
+        "kmt_votes": 10473,
+        "dpp_votes": 9836,
+        "tpp_votes": 7356,
+        "total_votes": 27665,
+        "kmt_rate": 37.9,
+        "dpp_rate": 35.6,
+        "tpp_rate": 26.6
+    },
+    "安樂區": {
+        "kmt_votes": 19140,
+        "dpp_votes": 17027,
+        "tpp_votes": 12886,
+        "total_votes": 49053,
+        "kmt_rate": 39.0,
+        "dpp_rate": 34.7,
+        "tpp_rate": 26.3
+    },
+    "信義區": {
+        "kmt_votes": 13630,
+        "dpp_votes": 10482,
+        "tpp_votes": 8991,
+        "total_votes": 33103,
+        "kmt_rate": 41.2,
+        "dpp_rate": 31.7,
+        "tpp_rate": 27.2
+    }
+};
+
+window.keelungRealData = keelungRealData;
+console.log('✅ 基隆市真实选举数据已更新并加载'); 
