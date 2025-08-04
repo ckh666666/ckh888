4410a4777f62023440abdb6cab28ff1bfdbd2284
CKH <1991652575@qq.com>
2025-08-04 15:02:54 +0800

    Create taitung-recall-data.js

---
+++ taitung-recall-data.js
@@ -0,0 +1,243 @@
+/**
+ * 臺東縣大罷免詳細數據
+ * 數據來源：2025年7月26日大罷免投票
+ */
+
+// 臺東縣大罷免數據（全縣範圍）
+const taitungRecallData = {
+    "臺東市": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 13413,
+        "disagree_votes": 21908,
+        "valid_votes": 35321,
+        "total_voters": 67250,
+        "total_eligible": 127000,
+        "turnout_rate": 52.50,
+        "agree_rate": 37.98,
+        "disagree_rate": 62.02
+    },
+    "成功鎮": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 697,
+        "disagree_votes": 1688,
+        "valid_votes": 2385,
+        "total_voters": 5285,
+        "total_eligible": 11700,
+        "turnout_rate": 45.13,
+        "agree_rate": 29.22,
+        "disagree_rate": 70.78
+    },
+    "關山鎮": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 747,
+        "disagree_votes": 1587,
+        "valid_votes": 2334,
+        "total_voters": 4870,
+        "total_eligible": 10150,
+        "turnout_rate": 47.94,
+        "agree_rate": 32.01,
+        "disagree_rate": 67.99
+    },
+    "卑南鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 1874,
+        "disagree_votes": 2657,
+        "valid_votes": 4531,
+        "total_voters": 8975,
+        "total_eligible": 17800,
+        "turnout_rate": 50.47,
+        "agree_rate": 41.36,
+        "disagree_rate": 58.64
+    },
+    "鹿野鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 888,
+        "disagree_votes": 1137,
+        "valid_votes": 2025,
+        "total_voters": 4405,
+        "total_eligible": 9580,
+        "turnout_rate": 45.99,
+        "agree_rate": 43.85,
+        "disagree_rate": 56.15
+    },
+    "池上鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 844,
+        "disagree_votes": 1376,
+        "valid_votes": 2220,
+        "total_voters": 4795,
+        "total_eligible": 10350,
+        "turnout_rate": 46.27,
+        "agree_rate": 38.02,
+        "disagree_rate": 61.98
+    },
+    "東河鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 589,
+        "disagree_votes": 838,
+        "valid_votes": 1427,
+        "total_voters": 3305,
+        "total_eligible": 7660,
+        "turnout_rate": 43.15,
+        "agree_rate": 41.28,
+        "disagree_rate": 58.72
+    },
+    "長濱鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 301,
+        "disagree_votes": 620,
+        "valid_votes": 921,
+        "total_voters": 1565,
+        "total_eligible": 3945,
+        "turnout_rate": 39.65,
+        "agree_rate": 32.68,
+        "disagree_rate": 67.32
+    },
+    "太麻里鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 968,
+        "disagree_votes": 1342,
+        "valid_votes": 2310,
+        "total_voters": 4845,
+        "total_eligible": 10150,
+        "turnout_rate": 47.69,
+        "agree_rate": 41.90,
+        "disagree_rate": 58.10
+    },
+    "大武鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 284,
+        "disagree_votes": 484,
+        "valid_votes": 768,
+        "total_voters": 1095,
+        "total_eligible": 2475,
+        "turnout_rate": 44.25,
+        "agree_rate": 36.98,
+        "disagree_rate": 63.02
+    },
+    "綠島鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 329,
+        "disagree_votes": 918,
+        "valid_votes": 1247,
+        "total_voters": 3540,
+        "total_eligible": 10050,
+        "turnout_rate": 35.23,
+        "agree_rate": 26.38,
+        "disagree_rate": 73.62
+    },
+    "海端鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 17,
+        "disagree_votes": 62,
+        "valid_votes": 79,
+        "total_voters": 197,
+        "total_eligible": 492,
+        "turnout_rate": 40.00,
+        "agree_rate": 21.52,
+        "disagree_rate": 78.48
+    },
+    "延平鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 33,
+        "disagree_votes": 64,
+        "valid_votes": 97,
+        "total_voters": 245,
+        "total_eligible": 619,
+        "turnout_rate": 39.59,
+        "agree_rate": 34.02,
+        "disagree_rate": 65.98
+    },
+    "金峰鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 12,
+        "disagree_votes": 42,
+        "valid_votes": 54,
+        "total_voters": 103,
+        "total_eligible": 196,
+        "turnout_rate": 52.43,
+        "agree_rate": 22.22,
+        "disagree_rate": 77.78
+    },
+    "達仁鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 16,
+        "disagree_votes": 48,
+        "valid_votes": 64,
+        "total_voters": 170,
+        "total_eligible": 452,
+        "turnout_rate": 37.65,
+        "agree_rate": 25.00,
+        "disagree_rate": 75.00
+    },
+    "蘭嶼鄉": {
+        "legislator": "黃建賓",
+        "party": "國民黨",
+        "isRecall": true,
+        "agree_votes": 111,
+        "disagree_votes": 118,
+        "valid_votes": 229,
+        "total_voters": 803,
+        "total_eligible": 2815,
+        "turnout_rate": 28.52,
+        "agree_rate": 48.47,
+        "disagree_rate": 51.53
+    }
+};
+
+// 立委信息列表
+const taitungLegislators = [
+    {
+        name: "黃建賓",
+        party: "國民黨",
+        district: "臺東縣",
+        regions: ["臺東市", "成功鎮", "關山鎮", "卑南鄉", "鹿野鄉", "池上鄉", "東河鄉", "長濱鄉", "太麻里鄉", "大武鄉", "綠島鄉", "海端鄉", "延平鄉", "金峰鄉", "達仁鄉", "蘭嶼鄉"],
+        total_agree: 21123,
+        total_disagree: 34889,
+        total_valid: 56012,
+        turnout_rate: 49.62,
+        agree_rate: 37.72,
+        disagree_rate: 62.28
+    }
+];
+
+// 使用框架注册数据
+if (window.registerRecallData) {
+    window.registerRecallData('台东县', taitungRecallData, {
+        legislators: taitungLegislators
+    });
+}
+
+window.taitungRecallData = taitungRecallData;
+window.taitungLegislators = taitungLegislators;
+console.log('✅ 臺東縣大罢免详细数据已加载'); 
